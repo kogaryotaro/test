c825ee558ad1a29258c4c1400ad2b8556e745f5a
kogaryotaro <ryotaroran531@gmail.com>
2023-09-26 22:19:39 +0900

    コメント

---
+++ 02_演習課題/script1.js
@@ -0,0 +1,100 @@
+//問1//
+document.write("問１");
+document.write("<br>");
+for(var i = 0; i < 5; i++){
+    document.write("★");
+}
+document.write("<br>");
+document.write("<br>");
+
+//問2//
+document.write("問2");
+document.write("<br>");
+for(var i = 1; i <= 2; i++){
+    for(var j = 1; j <= 3; j++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+//問3//
+document.write("問3");
+document.write("<br>");
+for(var i = 1; i <= 2; i++){
+    for(var j = 1; j <= 5; j++){
+        document.write("☆");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+//問4//
+document.write("問4");
+document.write("<br>");
+for(var i = 1; i <= 4; i++){
+    for(var j = 1; j <= 5; j++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+//問5//
+document.write("問5");
+document.write("<br>");
+for(var i = 1; i <= 4; i++){
+    for(var j = 1; j <= 3; j++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+//問6//
+document.write("問6");
+document.write("<br>");
+for(var i = 1; i <= 3; i++){
+    for(var j = 1; j <= 3; j++){
+        if(j % 2 == 0){
+            document.write("☆");
+        }else{
+            document.write("★");
+        }
+    }document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+//問7//
+document.write("問7");
+document.write("<br>");
+for(var i = 1; i <= 4; i++){
+    for(var j = 1; j <= 5; j++){
+        if(j % 2 != 1){
+            document.write("☆");
+        }else{
+            document.write("★");
+        }
+    }document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+//問8//
+document.write("問8");
+document.write("<br>");
+for(var i = 1; i <= 5; i++){
+    for(var j = 1; j <= i; j++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+
+
+
